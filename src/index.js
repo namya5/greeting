8b576b5b559ad6cbdf93915d8877e7218ac95274
namya5 <77522983+namya5@users.noreply.github.com>
2023-07-05 15:42:19 +0530

    Delete src directory

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-//changes as the time of day changes we change the text display accordingly 
-
-ReactDOM.render(<App />,document.getElementById("root"));
-

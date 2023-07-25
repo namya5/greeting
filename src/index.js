c6d8dd0228f9898647b7f1cac67dfe9572e91104
Namya Arora <aroranamya5@gmail.com>
2023-07-25 16:20:43 +0530

    feat: changed directory

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+//changes as the time of day changes we change the text display accordingly 
+
+ReactDOM.render(<App />,document.getElementById("root"));
+
